e287deee4b4c90a6aff9b2e376ddd3f86a63bf1f
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2022-04-27 14:57:36 -0400

    created

---
+++ rosterfullstack/src/main/java/com/hjp/rosterfullstack/RosterfullstackApplication.java
@@ -0,0 +1,13 @@
+package com.hjp.rosterfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosterfullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosterfullstackApplication.class, args);
+	}
+
+}
---
+++ rosterfullstack/src/test/java/com/hjp/rosterfullstack/RosterfullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjp.rosterfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosterfullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
